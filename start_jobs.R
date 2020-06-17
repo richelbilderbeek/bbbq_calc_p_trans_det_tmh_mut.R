4a944c3375531243f82377a1425e55f6b77fd618
richelbilderbeek <richel@richelbilderbeek.nl>
2020-06-17 13:09:49 +0200

    Prepare for Peregrine

--- start_jobs.R
+++ start_jobs.R
@@ -12,6 +12,11 @@ for (peptide_index in seq_len(n_peptides)) {
   for (mhc_haplotype in mhc_haplotypes) {
     filename <- paste0(i, ".csv")
     cmds <- c("Rscript", "calc_p_trans.R", filename, protein_sequence, mhc_haplotype, n_adjancent_sequences, percentile)
+
+    if (peregrine::is_on_peregrine()) {
+      cmds <- c("sbatch", cmds)
+    }
+
     system2(cmds[1], cmds[-1])
     i <- i + 1
   }
