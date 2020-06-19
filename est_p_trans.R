7fa910bb6a16fa3651f12776ccc08f9ac6ac1d75
richelbilderbeek <richel@richelbilderbeek.nl>
2020-06-19 17:38:54 +0200

    EOD

--- est_p_trans.R
+++ est_p_trans.R
@@ -24,7 +24,7 @@ for (i in seq_len(n_files)) {
 }
 
 
-readr::write_csv(t, "p_trans_p_0_05.csv")
+readr::write_csv(t, "p_trans_p_0_02.csv")
 p_trans <- mean(t$p)
 min(t$p)
 mean(t$p)
@@ -34,7 +34,7 @@ library(dplyr)
 
 readr::write_csv(
   t %>% group_by(mhc_haplotype) %>% summarise(max(p)),
-  "p_trans_p_0_05_max.csv"
+  "p_trans_p_0_02_max.csv"
 )
 
 n_peptides <- length(unique(t$protein_sequence))
@@ -46,4 +46,4 @@ ggplot2::ggplot(t, aes(x = mhc_haplotype, y = p)) +
     caption = glue::glue(
       "Number of peptides: {n_peptides}, percentile: {percentile}, average: {p_trans}"
     )
-  ) + ggsave("p_trans_0_05.png", width = 7, height = 7)
+  ) + ggsave("p_trans_0_02.png", width = 7, height = 7)
